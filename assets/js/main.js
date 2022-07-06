51aa2742b6d366a67186f10f4a94eccc95f3b12c
paula garcia <paulagarcia1306@gmail.com.ar>
2022-07-05 23:23:26 -0300

    interactivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
-let nombre = prompt ("Ingrese su nombre");
-let gracia = nombre;
-let pedidoConcatenar="";
+// consultar la cobertura en el precio de medicamentos
+let montoIngresado = parseInt (prompt("Ingrese precio de medicamento a consultar"));
+let montoPagar
 
+ const porcentaje = [20, 30, 40, 50];
+ for (let i =0; i<4 ; i++) {
+ if (montoIngresado <=0) {
+    alert ("Ingrese un monto valido por favor!")
+    break
+ }
 
-while (pedidoConcatenar != "esc"){
-pedidoConcatenar = prompt ("Ingrese los productos que necesite en este espacio");
-resultado = gracia + " solicitaste los siguientes productos: " + pedidoConcatenar;
-alert ("Hola, " + resultado);
-}
-
-
+ montoPagar = montoIngresado * porcentaje [i] / 100
+ alert ("Con un descuento del " + porcentaje[i] + "%, Pagas tu medicamento $" + montoPagar);
+ }
